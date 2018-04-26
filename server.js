795f2407e4462f1e448a80c8861d9c0ee815564d
Ajith55 <ajithchowdary96@gmail.com>
2018-04-26 19:32:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,15 +11,15 @@ app.get('/', function (req, res) {
 
 app.get('/article-one' , function(req, res){
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-})
+});
 
 app.get('/article-two' , function(req, res){
     res.send('Article two is requested and willbe served here');
-})
+});
 
 app.get('/article-three' , function(req, res){
     res.send('Article three is requested and willbe served here');
-})
+});
 
 
 
