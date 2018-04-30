34aa291182148f60725b37632f62aadf342b09aa
Ajith55 <ajithchowdary96@gmail.com>
2018-04-30 22:20:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,13 +89,6 @@ app.get('/:articleName' , function(req, res){
     res.send(createtemplate(articls[articleName]));
 });
 
-app.get('/article-two' , function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three' , function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 
 
 
